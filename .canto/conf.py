1db65a63a842e5d7e10a454f233b47078f07f494
Albert Decatur <adecatur@aiddata.org>
2014-06-10 12:02:14 -0400

    add canto feed reader config

---
+++ .canto/conf.py
@@ -0,0 +1,6 @@
+link_handler("links \"%u\"", text=True)
+image_handler("feh -ZF \"%u\"", fetch=True)
+link_handler("evince \"%u\"", fetch=True, ext="pdf")
+
+add("http://aiddata.org/blog/rss.xml")
+#add("http://reddit.com/.rss")
